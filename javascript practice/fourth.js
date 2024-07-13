0b549f434dbfc209f8d3f24595ee9efd719e609d
Jashandeep <jdeepsingh810@gmail.com>
2024-07-13 18:26:41 +0530

    hii

--- javascript practice/fourth.js
+++ javascript practice/fourth.js
@@ -8,8 +8,21 @@
 
 //  for in
 
- let heros = ["jahsna","saimen","rohit"];
+//  let heros = ["jahsna","saimen","rohit"];
 
- for (hero of heros) {
-     console.log(hero);
- }
+//  for (hero of heros) {
+//      console.log(hero);
+//  }
+
+
+// remove first company
+// remove facebook
+// add uber and netflix
+
+// let companies = ['Google','Facebook','Amazon','Microsoft','Apple'];
+
+// companies.shift();
+
+// console.splice(1,0,'Twitter');
+
+// console.push('Uber','Netflix');
--- javascript practice/practice4.js
+++ javascript practice/practice4.js
@@ -13,18 +13,71 @@
 
 
 
-let items = [250,645,300,900,50];
-let i = 0;
-for(let val of items){
+// let items = [250,645,300,900,50];
+// let i = 0;
+// for(let val of items){
  
    
-    let offer = val / 10;
-    items[i] = items[i] - offer;
-    console.log(`After 10% offer: ${items[i]}`);
-    i++;
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`After 10% offer: ${items[i]}`);
+//     i++;
 
-}
-console.log(items)
+// }
+// console.log(items)
 
+
+// Push
+
+// let foodItems = ['potato','tomato','onion','carrot'];
+// foodItems.push('apple');
+// console.log(foodItems);
+
+
+// Pop
+
+// let foodItems = ['potato','tomato','onion','carrot'];
+// foodItems.pop();
+// console.log(foodItems.toString());
+
+
+// Concat
+
+// let marvel_heroes = ['Ironman','Thor','Hulk','Captain America'];
+// let dc_heroes = ['Batman','Superman','Wonder'];
+// let heroes = marvel_heroes.concat(dc_heroes);
+// console.log(heroes);
+
+//add to start shifting
+
+// let marvel_heroes = ['Ironman','Thor','Hulk','Captain America'];
+
+// marvel_heroes.unshift('Black Widow');
+
+// let marvel_heroes = ['Ironman','Thor','Hulk','Captain America'];
+
+// let val= marvel_heroes.shift()
+// console.log(val);
+
+// slice
+
+// let marvel_heroes = ['Ironman','Thor','Hulk','Captain America'];
+
+// let heroes = marvel_heroes.slice(1,3);
+
+// Splice
+
+// let arr =['1','2','3','4','5','6','7','8','9','10'];
+
+// arr.splice(2,3,'a','b','c');
+
+// add element
+// arr.splice(2,0,101);
+
+// delete element
+// arr.splice(2,1);
+
+// replace element
+// arr.splice(2,1,101);
 
 
