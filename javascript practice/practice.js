f2256f497f6b58fd8ce957ca8a384081d79fe6df
Jashandeep <jdeepsingh810@gmail.com>
2024-08-30 11:35:57 +0530

    comit

---
+++ javascript practice/practice.js
@@ -0,0 +1,13 @@
+// var myArray = [1, 2, 3, 4, 5];
+
+// myArray.splice(1, 2);
+// console.log(myArray); // [1, 4, 5]
+
+// myArray.splice(4, 0, 2, 3);
+
+var myArray = [1, 2, 3, 4, 5];
+
+myArray.every((p)=> {
+    console.log(p>15);
+
+})
